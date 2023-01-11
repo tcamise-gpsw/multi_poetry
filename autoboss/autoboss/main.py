8f2eac77f72bae7f8c1d98ed3b6e2c7619f6af10
Tim Camise <tcamise@gopro.com>
2023-01-11 15:15:33 -0800

    Add autoboss and cleanup package includes

--- autoboss/autoboss/main.py
+++ autoboss/autoboss/main.py
@@ -1,11 +1,11 @@
 import cowsay
 
-from shared.toolbox.package_a.module_a import do_a_thing as test_a
-from shared.toolbox.package_b.module_b import do_a_thing as test_b
+from toolbox.package_a.main import do_a_thing as test_a
+from toolbox.package_b.main import do_a_thing as test_b
 
 
 def main() -> None:
     test_a()
     test_b()
 
-    cowsay.dragon("I am autoboss")
+    cowsay.dragon("I am PYWSDK")
